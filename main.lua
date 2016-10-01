42cdb0b9f137f96d1df789519f2ae015bffa9691
Ryker Blunck <rykerblunck@gmail.com>
2016-09-30 21:16:13 -0600

    can move left and right and draws (empty) board

--- main.lua
+++ main.lua
@@ -3,6 +3,17 @@
 local shapes = require('shapes')
 local current_shape = shapes[1]
 local orientation = 1
+local board = {}
+local drop_timer = 0
+local top = 0
+local left = 0
+
+for row = 1, 16 do
+  board[row] = {}
+  for column = 1, 10 do
+    board[row][column] = 0
+  end
+end
 
 function love.load()
   love.window.setMode(400, 520)
@@ -23,11 +34,38 @@ function love.keypressed(key)
     rotate_shape()
   elseif key == 'down' then
     rotate_shape(true)
+  elseif key == 'left' then
+    left = left - 1
+  elseif key == 'right' then
+    left = left + 1
   end
 end
 
-function love.draw()
+function love.update(dt)
+  drop_timer = drop_timer + dt
+  if drop_timer > .5 then
+    top = top + 1
+    drop_timer = 0
+  end
+end
+
+local function draw_current_shape()
   for row_index, row in ipairs(current_shape[orientation]) do
+    for column_index, column in ipairs(row) do
+      if column == 1 then
+        love.graphics.rectangle(
+          'fill',
+          (column_index - 1 + left) * 10,
+          (row_index - 1 + top) * 10,
+          9,
+          9)
+      end
+    end
+  end
+end
+
+local function draw_board()
+  for row_index, row in ipairs(board) do
     for column_index, column in ipairs(row) do
       if column == 1 then
         love.graphics.rectangle(
@@ -39,4 +77,9 @@ function love.draw()
       end
     end
   end
+end
+
+function love.draw()
+  draw_current_shape()
+  draw_board()
 end
