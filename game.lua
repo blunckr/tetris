2af69efcdf3919ba99c8b835788ed47ec6b659e8
Ryker Blunck <rykerblunck@gmail.com>
2016-10-10 12:46:44 -0600

    quick drop

--- game.lua
+++ game.lua
@@ -16,6 +16,26 @@ function Game.new_shape(self)
   self.shape = Shape.new()
 end
 
+function Game.drop_shape(self)
+  local valid
+  self.shape:drop(function(next_shape)
+    valid = self.board:shape_is_valid(next_shape)
+    if not valid then
+      self.board:eat_blocks(self.shape:blocks())
+      self:new_shape()
+    end
+    return valid
+  end)
+  return valid
+end
+
+function Game.quick_drop(self)
+  local settled = false
+  while settled == false do
+    settled = not self:drop_shape()
+  end
+end
+
 function Game.keypressed(self, key)
   local validate = function(next_shape)
     return self.board:shape_is_valid(next_shape)
@@ -28,6 +48,8 @@ function Game.keypressed(self, key)
     self.shape:move_x('left', validate)
   elseif key == 'right' then
     self.shape:move_x('right', validate)
+  elseif key == 'space' then
+    self:quick_drop()
   end
 end
 
@@ -35,14 +57,7 @@ function Game.update(self, dt)
   self.drop_timer = self.drop_timer + dt
   if self.drop_timer > .5 then
     self.drop_timer = 0
-    self.shape:drop(function(next_shape)
-      local valid = self.board:shape_is_valid(next_shape)
-      if not valid then
-        self.board:eat_blocks(self.shape:blocks())
-        self:new_shape()
-      end
-      return valid
-    end)
+    self:drop_shape()
   end
 end
 
